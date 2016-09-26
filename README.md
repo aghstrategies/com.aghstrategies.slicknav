d7d841e5dc7b2177e291887b8e0bf9719a2a833c
Alice Frumin <alice@aghstrategies.com>
2016-09-25 23:32:08 -0400

    first two levels of nav working

---
+++ README.md
@@ -0,0 +1,15 @@
+This Extension
+--------------
+
+http://slicknav.com/
+
+
+Things to do:
+
++ Same permsissions as for seeing other admin bar
++ clear out files that aren't needed
++ Make divs work ex: contributions -pledges ... jquery .clone?
++ use font awesome chevron arrows for open and close
++ style slightly
++ full width or 97%?
++ search dropdown when small
---
+++ js/slicknav.js
@@ -0,0 +1,17 @@
+CRM.$(function ($) {
+  $('#civicrm-menu').slicknav({
+    label: '',
+    duration: 1000,
+    removeClasses: 'true',
+    removeStyles: 'true',
+    // openedSymbol:'fa fa-shield',
+    // closedSymbol: '&#xf078;',
+    // removeIds: 'true',
+    appendTo: '#civicrm-menu',
+    // duplicate: 'false',
+    // allowParentLinks: 'TRUE',
+    // nestedParentLinks: 'true',
+    // easingOpen: 'easeOutBounce',
+    closeOnClick: 'true',
+  });
+});
